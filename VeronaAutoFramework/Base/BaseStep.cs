f2ae797c80aa0aa4e6f73072a4aade2de979e912
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-20 11:42:21 +0100

    first framework commit

---
+++ VeronaAutoFramework/Base/BaseStep.cs
@@ -0,0 +1,20 @@
+﻿using VeronaAutoFramework.Config;
+using VeronaAutoFramework.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace VeronaAutoFramework.Base
+{
+    public class BaseStep : Base
+    {
+        public BaseStep(ParallelConfig parellelConfig) : base(parellelConfig)
+        {
+        }
+
+      
+    }
+}
---
+++ VeronaAutoFramework/Base/DriverContext.cs
@@ -0,0 +1,23 @@
+﻿namespace VeronaAutoFramework.Base
+{
+    public class DriverContext
+    {
+
+        public readonly ParallelConfig _parellelConfig;
+
+        public DriverContext(ParallelConfig parellelConfig)
+        {
+            _parellelConfig = parellelConfig;
+        }
+
+
+        public void GoToUrl(string url)
+        {
+            _parellelConfig.Driver.Url = url;
+        }
+
+
+        public static Browser Browser { get; set; }
+
+    }
+}
